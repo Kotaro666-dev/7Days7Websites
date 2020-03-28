96c0d4719a4c5ee2e4a94d924b50b98e3fc42652
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-03-28 15:25:18 +0900

    Fix errorHandling on js file

--- day3/index.js
+++ day3/index.js
@@ -1,12 +1,18 @@
-function checkEmailValidation() {
+const buttonClicked = document.getElementById("button");
+
+buttonClicked.addEventListener("click", function() {
   const mailFormat = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
   const emailInputText = document.getElementById("email").value;
 
   if (emailInputText.match(mailFormat)) {
     document.getElementById("errorMsg").style.visibility = "hidden";
     document.getElementById("email").classList.remove("error-msg");
+
+    return true;
   } else {
     document.getElementById("errorMsg").style.visibility = "visible";
     document.getElementById("email").classList.add("error-msg");
+
+    return false;
   }
-}
+});
