55f317b94125911d69dcf5081491d5d8a584eb72
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-03-28 09:35:18 +0900

    Fix index.js: let -> const & deleted comments

--- day2/index.js
+++ day2/index.js
@@ -1,10 +1,10 @@
-let clickedSubmit = document.getElementById("button");
+const clickedSubmit = document.getElementById("button");
 
 clickedSubmit.addEventListener("click", function() {
   //   let firstNameFormat = /^$/;
   //   let lastNameFormat = /^$/;
-  let mailFormat = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
-  let passwordFormat = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+  const mailFormat = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+  const passwordFormat = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
 
   let firstNameInputText = document.getElementById("firstName").value;
   let lastNameInputText = document.getElementById("lastName").value;
@@ -14,33 +14,25 @@ clickedSubmit.addEventListener("click", function() {
   if (firstNameInputText.length != 0) {
     document.getElementById("firstName").classList.remove("error-icon");
     document.getElementById("errorMsgFirst").style.visibility = "hidden";
-
-    // return true;
   } else {
     document.getElementById("firstName").classList.add("error-icon");
     document.getElementById("errorMsgFirst").style.visibility = "visible";
-    // return false;
   }
 
   if (lastNameInputText.length != 0) {
     document.getElementById("lastName").classList.remove("error-icon");
     document.getElementById("errorMsgLast").style.visibility = "hidden";
-    // return true;
   } else {
     document.getElementById("lastName").classList.add("error-icon");
     document.getElementById("errorMsgLast").style.visibility = "visible";
-    // return false;
   }
 
   if (emailInputText.match(mailFormat)) {
     document.getElementById("email").classList.remove("error-icon");
     document.getElementById("errorMsgEmail").style.visibility = "hidden";
-
-    // return true;
   } else {
     document.getElementById("email").classList.add("error-icon");
     document.getElementById("errorMsgEmail").style.visibility = "visible";
-    // return false;
   }
 
   if (passwordInputText.match(passwordFormat)) {
@@ -53,3 +45,13 @@ clickedSubmit.addEventListener("click", function() {
 
   return 0;
 });
+
+// function successHandling(iconId, msgId) {
+//   document.getElementById(iconId).classList.remove("error-icon");
+//   document.getElementById(msgId).style.visibility = "hidden";
+// }
+
+// function errorHandling(iconId, msgId) {
+//   document.getElementById(iconId).classList.add("error-icon");
+//   document.getElementById(msgId).style.visibility = "visible";
+// }
