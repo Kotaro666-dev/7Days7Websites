75aaaaa16216d3abb0c2ad99e5fa731d5cd5d6fa
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-02 19:56:13 +0900

    Fix hamburger nav bar

--- day7/index.js
+++ day7/index.js
@@ -1,13 +1,16 @@
 const hamburger = document.getElementById("hamburger");
 const close = document.getElementById("close");
-const navigation = document.getElementById("navigationPage");
+const navigation = document.getElementById("navigationPageMobile");
 
 hamburger.style.visibility = "visible";
+navigation.style.visibility = "hidden";
 
 function changeNavIcon() {
   if (hamburger.style.visibility == "visible") {
     hamburger.style.visibility = "hidden";
     close.style.visibility = "visible";
+    navigation.style.visibility = "visible";
+
     navigation.style.visibility = "visible";
   } else {
     hamburger.style.visibility = "visible";
