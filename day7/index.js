9df0ce2d5ec42c614eac5a639e1529791d2d0897
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-02 13:15:54 +0900

    Create hamburger nabar

---
+++ day7/index.js
@@ -0,0 +1,18 @@
+const hamburger = document.getElementById("hamburger");
+const close = document.getElementById("close");
+const navigation = document.getElementById("navigationPage");
+
+function changeNavIcon() {
+  if (hamburger.style.visibility == "visible") {
+    hamburger.style.visibility = "hidden";
+    close.style.visibility = "visible";
+    navigation.style.visibility = "visible";
+  } else {
+    hamburger.style.visibility = "visible";
+    close.style.visibility = "hidden";
+    navigation.style.visibility = "hidden";
+  }
+}
+
+hamburger.addEventListener("click", changeNavIcon);
+close.addEventListener("click", changeNavIcon);
